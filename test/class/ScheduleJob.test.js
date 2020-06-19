d1592647915c806b9e4dc72af424aa63ffe5a9a7
Felipe Jardim <jardim.felipe@hotmail.com>
2020-06-19 13:28:44 -0300

    Fix tests

--- test/class/ScheduleJob.test.js
+++ test/class/ScheduleJob.test.js
@@ -1,7 +1,7 @@
 const ScheduleJob = require("../../class/ScheduleJob.js");
 
 describe("./ScheduleJob parameters validation", () => {
-  it("creates a instance with all parameters", () => {
+  it("should create a instance with all parameters", () => {
     const mockJob = {
       id: 1,
       description: "Importação de arquivos de fundos",
@@ -13,5 +13,13 @@ describe("./ScheduleJob parameters validation", () => {
     expect(mockInstance.description).toBe("Importação de arquivos de fundos");
     expect(mockInstance.maxConclusionDate).toBe("2019-11-10 12:00:00");
     expect(mockInstance.estimateTime).toBe("2 horas");
+  });
+  it("should trhow error when a parameter or more is missing", () => {
+    const mockJob = {
+      description: "Importação de arquivos de fundos",
+      maxConclusionDate: "2019-11-10 12:00:00",
+      estimateTime: "2 horas",
+    };
+    expect(() => new ScheduleJob(mockJob)).toThrow(Error);
   });
 });
